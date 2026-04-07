42ecf7f6de4b77ec7190e8178c4402a169bd906f
oumar-bah <ob2770048@gmail.com>
2026-04-08 00:43:05 +0200

    Update App.js to import and display LanguageBar and Header with their CSS.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-import LanguageSelector from './components/LanguageSelector';
+import LanguageBar from './components/LanguageBar';
+import Header from './components/Header';
+import './components/LanguageBar.css';
+import './components/Header.css';
 
 function App() {
   return (
     <div>
-      <LanguageSelector />
+      <LanguageBar />
       <Header />
       {/* Other components and routing logic */}
     </div>
