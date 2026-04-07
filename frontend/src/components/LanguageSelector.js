57475ecdf42801ebf4776c1cbaae1469e7f62aed
oumar-bah <ob2770048@gmail.com>
2026-04-08 00:07:44 +0200

    Copy LanguageSelector.js to frontend/src/components/LanguageSelector.js

---
+++ frontend/src/components/LanguageSelector.js
@@ -0,0 +1,10 @@
+// language selector component code here
+function LanguageSelector() {
+    return (
+        <div>
+            {/* Your language selector UI here */}
+        </div>
+    );
+}
+
+export default LanguageSelector;
